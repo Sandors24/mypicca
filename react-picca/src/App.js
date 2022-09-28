0e55136b57770ed94d8a710c186bdec2f213b4bf
Sandor Sandor <sandors0000@gmail.com>
2022-09-29 00:05:45 +0300

    picca4

---
+++ react-picca/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import  "./scss/app.scss"
+
+import { Categories } from './components/Categories'
+import { Sort } from './components/Sort'
+import {Header} from "./components/Header"
+import { PizzaBlock } from './components/PizzaBlock'
+import pizzas from "./assets/pizza.json"
+
+console.log(pizzas);
+
+function App() {
+  return (
+
+<>
+<div className="wrapper">
+<Header/> 
+  <input type="text" />
+ 
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+          <Categories/>
+    <Sort/>
+             
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+{
+  pizzas.map((obj)=>(<PizzaBlock {...obj}/>))
+}
+
+
+
+
+
+<PizzaBlock tittle="irlandskiy"/>
+
+          
+  
+  </div>
+
+ 
+</div> 
+
+</div>
+</div>        
+        
+      
+    
+</>
+
+
+  )
+}
+
+export default App;
---
+++ react-picca/src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const Categories = () => {
+  const [activindex,setActivIndex] =React.useState(0)
+
+  const  categories = [
+    "Все",
+    "ясМные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые"
+  ]
+
+  const onClickCategory=(index)=>{
+    setActivIndex(index)
+  }
+  return (
+    <div className="categories">
+    <ul>
+    {categories.map((value,i)=>(
+      <li onClick={()=>onClickCategory(i)}  className={activindex===i?"active":""}>{value}</li>
+      
+    ))}
+    </ul>
+  </div>
+  )
+}
